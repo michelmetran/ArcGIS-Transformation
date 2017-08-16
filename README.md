2376a79789fb653d41e1ac71372d296c7f563bc9
michelmetran <michelmetran@gmail.com>
2017-08-16 01:14:48 -0300

    Final

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # ArcGIS-Transformation
-Cria parâmetros de transformações entre os datum utilizados no Brasil conforme dados oficiais.
+Cria parâmetros de transformações entre os datum comumente utilizados no Brasil conforme dados oficiais.
 
 ## Para que serve?
 Existe muita confusão na converção e projeção de arquivos espaciais (*shapefiles* e *feature class*) no ArcGIS.
+Por *default*, o ArcGIS dispõe de 14 maneiras diferentes para converter feições entre SAD69 e WGS84. A versão mais correta para se utilizar é a *SAD_1969_To_WGS_1984_14*.
+
+*E para outros datum?* O objetivo desse script é facilitar tais conversões, criando no ArcGIS, os parâmetros de transformação corretos, definidos pelo IBGE e outras instituições de pesquisa.
 
 ## Como usar?
 Fazer o download (ou cópia) do arquivo 'Transformation.py' e executar.
@@ -16,3 +19,15 @@ Testado com as versões 10.3 e 10.5.
 Sobre a [criação de transformações entre datum customizada no ArcGIS](http://desktop.arcgis.com/en/arcmap/10.5/tools/data-management-toolbox/create-custom-geographic-transformation.htm).
 
 Sobre [questões de geodésia definidas pelo IBGE](http://www.ibge.gov.br/home/geociencias/geodesia/pmrg/faq.shtm).
+
+Sobre [transformação entre referenciais geodésicos definidos pela Engenharia Cartográfica da UFRGS](http://www.ufrgs.br/engcart/Teste/refer_exp.html).
+
+Sobre os [parâmetros de transformações entre datum que existem, por *default*, no ArcGIS](http://help.arcgis.com/en/arcgisdesktop/10.0/help/003r/pdf/geographic_transformations.pdf).
+
+## Autor
+* **Michel Metran** - *Ideia Inicial* - [site](https://michelmetran.com)
+
+Veja também a lista de [colaboradores](https://github.com/michelmetran/ArcGIS-Transformation/graphs/contributors) que auxiliaram nesse projeto.
+
+## License
+Esse projeto é licenciado sob a MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- Transformation.py
+++ Transformation.py
@@ -1,8 +1,7 @@
 # ########################################################################################
 # Python Script to ArcGIS
 # Description: Creates a custom geographic transformation in the default directory.
-# C:\Users\[USER]\AppData\Roaming\ESRI\Desktop10.5\ArcToolbox\CustomTransformations
-# Michel Metran (MichelMetran.com)
+# C:\Users\[USER]\AppData\Roaming\ESRI\Desktop[VERSION]\ArcToolbox\CustomTransformations
 
 # ########################################################################################
 # Import System Modules
@@ -76,6 +75,84 @@ try:
     # Create a Spatial Reference Objects (Input/Output)
     inGCS = arcpy.SpatialReference("South American Datum 1969")
     outGCS = arcpy.SpatialReference("SIRGAS 2000")
+    
+    customGeoTransfm = "GEOGTRAN[METHOD['Geocentric_Translation'],\
+            PARAMETER['X_Axis_Translation',"+str(x)+"],\
+            PARAMETER['Y_Axis_Translation',"+str(y)+"],\
+            PARAMETER['Z_Axis_Translation',"+str(z)+"]]"
+    
+    arcpy.CreateCustomGeoTransformation_management(geoTransfmName,
+                                                   inGCS, outGCS,
+                                                   customGeoTransfm)
+    
+except arcpy.ExecuteError:
+    print arcpy.GetMessages()
+
+# ########################################################################################
+# Transformation between SIRGAS2000 to WGS84
+try:
+    geoTransfmName = "SIRGAS2000_para_WGS84"
+    
+    # Set Parameters
+    x = +0.478
+    y = +0.491
+    z = -0.297
+    
+    # Create a Spatial Reference Objects (Input/Output)
+    inGCS = arcpy.SpatialReference("SIRGAS 2000")
+    outGCS = arcpy.SpatialReference("WGS 1984")
+    
+    customGeoTransfm = "GEOGTRAN[METHOD['Geocentric_Translation'],\
+            PARAMETER['X_Axis_Translation',"+str(x)+"],\
+            PARAMETER['Y_Axis_Translation',"+str(y)+"],\
+            PARAMETER['Z_Axis_Translation',"+str(z)+"]]"
+    
+    arcpy.CreateCustomGeoTransformation_management(geoTransfmName,
+                                                   inGCS, outGCS,
+                                                   customGeoTransfm)
+    
+except arcpy.ExecuteError:
+    print arcpy.GetMessages()
+
+# ########################################################################################
+# Transformation between SIRGAS2000 to Córrego Alegre
+try:
+    geoTransfmName = "SIRGAS2000_para_CórregoAlegre"
+    
+    # Set Parameters
+    x = +206.048
+    y = -168.279
+    z = +3.823
+    
+    # Create a Spatial Reference Objects (Input/Output)
+    inGCS = arcpy.SpatialReference("SIRGAS 2000")
+    outGCS = arcpy.SpatialReference("Corrego Alegre")
+    
+    customGeoTransfm = "GEOGTRAN[METHOD['Geocentric_Translation'],\
+            PARAMETER['X_Axis_Translation',"+str(x)+"],\
+            PARAMETER['Y_Axis_Translation',"+str(y)+"],\
+            PARAMETER['Z_Axis_Translation',"+str(z)+"]]"
+    
+    arcpy.CreateCustomGeoTransformation_management(geoTransfmName,
+                                                   inGCS, outGCS,
+                                                   customGeoTransfm)
+    
+except arcpy.ExecuteError:
+    print arcpy.GetMessages()
+    
+# ########################################################################################
+# Transformation between Córrego Alegre to WGS84
+try:
+    geoTransfmName = "CórregoAlegre_para_WGS84"
+    
+    # Set Parameters
+    x = -205.57
+    y = +168.77
+    z = -4.12
+    
+    # Create a Spatial Reference Objects (Input/Output)
+    inGCS = arcpy.SpatialReference("Corrego Alegre")
+    outGCS = arcpy.SpatialReference("WGS 1984")
     
     customGeoTransfm = "GEOGTRAN[METHOD['Geocentric_Translation'],\
             PARAMETER['X_Axis_Translation',"+str(x)+"],\
