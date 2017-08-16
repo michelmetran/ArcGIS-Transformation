fc5e80a310a74f70feac1d9c99b89be02e727a49
michelmetran <michelmetran@gmail.com>
2017-08-15 23:11:37 -0300

    Revision

--- README.md
+++ README.md
@@ -1,18 +1,13 @@
 # ArcGIS-Transformation
-Cria parâmetros de Transformações entre os datuns utilizados no Brasil
+Cria parâmetros de transformações entre os datuns utilizados no Brasil conforme dados oficiais.
 
 ## Para que serve?
-Existe muita confusão entre Teste!!
-
-sdsdsds
-
-
-
+Existe muita confusão na converção e projeção de arquivos espaciais (shapefiles e featura class) no ArcGIS.
 
 ## Como usar?
 Fazer o download (ou cópia) do arquivo 'Transformation.py' e executar.
 
 ## Pré-requisitos
-ArcGIS instalado;
+- ArcGIS instalado;
 
 ## Referências
--- Transformation.py
+++ Transformation.py
@@ -1,13 +1,13 @@
-# Name: CreateCustomGeographicTransformation.py
 # Description: Creates a custom geographic transformation in the default directory.
-# http://desktop.arcgis.com/en/arcmap/10.3/tools/data-management-toolbox/create-custom-geographic-transformation.htm
-# Piracicaba, 19 de Setembro de 2016
+# References: http://desktop.arcgis.com/en/arcmap/10.3/tools/data-management-toolbox/create-custom-geographic-transformation.htm
 # Michel Metran (www.michelmetran.com)
-
+# ##############################################################################################
 
 # Import System Modules
 import arcpy
 
+
+# ##############################################################################################
 # Set the variables
 geoTransfmName = "SAD69_para_WGS84"
 
@@ -23,8 +23,7 @@ customGeoTransfm ="GEOGTRAN[METHOD['Geocentric_Translation'],PARAMETER['X_Axis_T
 # Aplly Fuction
 arcpy.CreateCustomGeoTransformation_management(geoTransfmName, inGCS, outGCS, customGeoTransfm)
 
-# ############################################################################
-
+# ##############################################################################################
 # Set the variables
 geoTransfmName = "SAD69_para_CorregoAlegre"
 
@@ -41,7 +40,6 @@ customGeoTransfm ="GEOGTRAN[METHOD['Geocentric_Translation'],PARAMETER['X_Axis_T
 arcpy.CreateCustomGeoTransformation_management(geoTransfmName, inGCS, outGCS, customGeoTransfm)
 
 # ############################################################################
-
 # Set the variables
 geoTransfmName = "SAD69_para_SIRGAS2000"
 
